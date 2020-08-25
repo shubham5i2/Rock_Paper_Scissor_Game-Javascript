3faf52817b2e17e85c04826f966d32bd867449fd
shubham5i2 <shubhamkumarsingh450@gmail.com>
2020-08-25 10:11:38 +0530

    commit

---
+++ app.js
@@ -0,0 +1,98 @@
+let userScore = 0;
+let computerScore = 0;
+const userScore_span = document.getElementById("user-score");
+const computerScore_span = document.getElementById("computer-score");
+const scoreBoard_div = document.querySelector(".score-board");
+const result_div = document.querySelector(".result > p");
+const rock_div = document.getElementById("r");
+const paper_div = document.getElementById("p");
+const scissors_div = document.getElementById("s");
+
+function getComputerChoice() {
+    const choices = ['r', 'p', 's'];
+    const randonNumber = Math.floor(Math.random() * 3);
+    return choices[randonNumber];
+}
+
+function convertToWord(letter) {
+    if (letter === 'r') {
+        return "Rock"
+    }
+    if (letter === 'p') {
+        return "Paper"
+    }
+    return "Scissors";
+}
+
+function win(userChoice, computerChoice) {
+    userScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sup();
+    const smallCompWord = "comp".fontsize(3).sup();
+    result_div.innerHTML = convertToWord(userChoice) + smallUserWord + " beats " + convertToWord(computerChoice) + smallCompWord + ". <span style = 'color:green; font-family:Acme, sans-serif;'>You win!</span>";
+    document.getElementById(userChoice).classList.add("green-glow");
+    setTimeout(() => {
+        document.getElementById(userChoice).classList.remove("green-glow");
+    }, 300);
+}
+
+function lose(userChoice, computerChoice) {
+    computerScore++;
+    userScore_span.innerHTML = userScore;
+    computerScore_span.innerHTML = computerScore;
+    const smallUserWord = "user".fontsize(3).sup();
+    const smallCompWord = "comp".fontsize(3).sup();
+    result_div.innerHTML = convertToWord(userChoice) + smallUserWord + " loses to " + convertToWord(computerChoice) + smallCompWord + ". <span style = 'color:red;font-family:Acme, sans-serif;'>You lose!</span>";
+    document.getElementById(userChoice).classList.add("red-glow");
+    setTimeout(() => {
+        document.getElementById(userChoice).classList.remove("red-glow");
+    }, 300);
+}
+
+function draw(userChoice, computerChoice) {
+    const smallUserWord = "user".fontsize(3).sup();
+    const smallCompWord = "comp".fontsize(3).sup();
+    result_div.innerHTML = convertToWord(userChoice) + smallUserWord + " equals " + convertToWord(computerChoice) + smallCompWord + ". <span style = 'color:gray;font-family:Acme, sans-serif;'>It's a draw!</span>";
+    document.getElementById(userChoice).classList.add("gray-glow");
+    setTimeout(() => {
+        document.getElementById(userChoice).classList.remove("gray-glow");
+    }, 300);
+}
+
+function game(userChoice) {
+    const computerChoice = getComputerChoice();
+    switch (userChoice + computerChoice) {
+        case "rs":
+        case "pr":
+        case "sp":
+            win(userChoice, computerChoice);
+            break;
+        case "rp":
+        case "ps":
+        case "sr":
+            lose(userChoice, computerChoice);
+            break;
+        case "rr":
+        case "pp":
+        case "ss":
+            draw(userChoice, computerChoice);
+            break;
+    }
+}
+
+function main() {
+    rock_div.addEventListener("click", () => game("r")) //arrow function
+
+    paper_div.addEventListener("click", function () { //anonymous function
+        game("p");
+    })
+
+    scissors_div.addEventListener("click", scissors) //normal function call
+}
+
+function scissors() {
+    game("s");
+}
+
+main();
